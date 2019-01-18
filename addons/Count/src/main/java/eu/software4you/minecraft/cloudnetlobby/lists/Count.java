b70000dec11f22bd7df6f40f48bfc3aad947b953
fluse1367 <joni.gibas@gmail.com>
2019-01-18 16:48:21 +0100

    done things

--- addons/Count/src/main/java/eu/software4you/minecraft/cloudnetlobby/lists/Count.java
+++ addons/Count/src/main/java/eu/software4you/minecraft/cloudnetlobby/lists/Count.java
@@ -1,17 +1,17 @@
 package eu.software4you.minecraft.cloudnetlobby.lists;
 
-import eu.software4you.minecraft.cloudnetlobby.addons.List;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Count extends List {
+public class Count extends eu.software4you.minecraft.cloudnetlobby.addons.List {
     public Count() {
         super("Count");
     }
 
     @Override
-    protected java.util.List<String> list(Player caller, String arg) {
+    protected List<String> list(Player caller, String arg) {
         java.util.List<String> li = new ArrayList<>();
         if (arg.contains(",")) {
             int a = Integer.valueOf(arg.split(",")[0]);
