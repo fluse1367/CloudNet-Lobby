275caccaf3ad4f939cf157a175f119f15e674098
fluse1367 <joni.gibas@gmail.com>
2019-01-22 17:41:07 +0100

    added HasPerm

--- addons/Player/src/main/java/eu/software4you/minecraft/cloudnetlobby/placeholders/Player.java
+++ addons/Player/src/main/java/eu/software4you/minecraft/cloudnetlobby/placeholders/Player.java
@@ -9,13 +9,18 @@ public class Player extends Placeholder {
 
     @Override
     protected String replace(org.bukkit.entity.Player caller, String arg) {
-        switch (arg) {
+        String cmd = arg.contains(":") ? arg.substring(0, arg.indexOf(":")) : arg;
+        arg = arg.substring(cmd.length());
+
+        switch (cmd) {
             case "Name":
                 return caller.getName();
             case "DisplayName":
                 return caller.getDisplayName();
             case "UUID":
                 return caller.getUniqueId().toString();
+            case "HasPerm":
+                return String.valueOf(caller.hasPermission(arg));
         }
         return null;
     }
